f69b9624cd2505dd3b8affc8f7f1202c0b0d9fda
DongJu-Na <ehdeld123@naver.com>
2025-04-17 09:55:45 +0900

    init 🛠️

---
+++ src/test/java/com/car/ndj/NdjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.car.ndj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NdjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
